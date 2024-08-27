ed55e9447e8ac37901bdb8bd23ff2b72fcc33b97
Mikhail Kravtsov <kr.mikki@yandex.ru>
2024-08-27 10:22:16 +0300

    Fix app tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,14 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppRepository } from './app.repository';
+import { PrismaModule } from './prisma/prisma.module';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, AppRepository],
     }).compile();
 
     appController = app.get<AppController>(AppController);
